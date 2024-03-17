98461eff83684354d1a5db0aa258fa00db4562e2
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-03-17 00:09:33 -0300

    projeto finalziado

--- projeto_usuarios/urls.py
+++ projeto_usuarios/urls.py
@@ -5,7 +5,7 @@ from app_cad_usuarios import views
 
 
 urlpatterns = [
-   path('', views.home,name='home'),
+   path('',views.home,name='home'),
    path('usuarios/', views.usuarios, name='listagem_usuarios')
 
 ]
